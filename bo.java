934d3f354a7d127068a79eed337c42f84beef646
Duongg <duongg@gmail.com>
2022-11-01 16:54:54 +0700

    Tao la thng moi tao sua class bo

--- bo.java
+++ bo.java
@@ -5,6 +5,8 @@ public class bo
     System.out.println("Booooooooooooooooooooooooooooooo nee !");
     System.out.println("Tao la thng cu va tao cung vay do");
     System.out.println("Tao la thng cu tao cung vay");
+   
+    System.out.println("Tao la thng moi tao sua phan bo");
    }
     
     
