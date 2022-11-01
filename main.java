007bd43fbe311706071c020feb3cf6a3942dfe4d
Duongg <duongg@gmail.com>
2022-11-01 15:32:05 +0700

    Them vao ham main cau .....hi tui la main ne

--- main.java
+++ main.java
@@ -3,5 +3,6 @@ public class main
     public static void main (String [] args)
     {
       System.out.println("Hello tui la main day");
+      System.out.println("Them hi tui la main ne");
     }
 }
